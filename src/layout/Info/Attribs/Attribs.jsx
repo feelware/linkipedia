01253efa82ae46705a0f33184a8aa8c3d141a1ac
miguelespp <carlos.espinoza24@unmsm.edu.pe>
2024-07-01 00:18:52 -0500

    feat: add dfs algorithm

--- src/layout/Info/Attribs/Attribs.jsx
+++ src/layout/Info/Attribs/Attribs.jsx
@@ -2,9 +2,15 @@ import {
   Tree,
   useTree
 } from '@mantine/core'
+import dfs from './dfs';
+import useData from '../../../store/useData';
 
 const Attribs = () => {
-  const tree = useTree()
+  const tree = useTree();
+  const { expandedItems, activeNode } = useData();
+  const attribs = dfs(expandedItems, activeNode);
+  console.log(attribs);
+
   
   return (
     <>
